662f9276c876e73ec2715aff3d46836da51c4a37
mkhan <relisource@makMashuk-2.local>
2022-06-02 10:48:09 +0600

    Log Api

---
+++ src/app.module.ts
@@ -0,0 +1,17 @@
+import { Module } from '@nestjs/common';
+import { MulterModule } from '@nestjs/platform-express/multer';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { LogsController } from './logs/controller/logs.controller';
+import { LogsService } from './logs/service/logs.service';
+
+@Module({
+  imports: [
+    MulterModule.register({
+      dest: './files',
+    })
+  ],
+  controllers: [AppController, LogsController],
+  providers: [AppService,LogsService],
+})
+export class AppModule {}
